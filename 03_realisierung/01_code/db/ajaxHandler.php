021fbed280f7d9a38037c938a06471cd31837d3b
Simonch8 <moeckli.soeckli@gmail.com>
2017-11-14 14:28:34 +0100

    Ajax Handler created

--- 03_realisierung/01_code/db/ajaxHandler.php
+++ 03_realisierung/01_code/db/ajaxHandler.php
@@ -1,5 +1,27 @@
 <?php
+// Set Session ID with ip of user and start the session
+session_id(str_replace(array('.', ':'), array('-', '-'), $_SERVER["REMOTE_ADDR"]));
+session_start();
+
+// Includes functions
 require_once "conn.php";
 
+// Store POST Variable in simple local variable and initialize new variable "oldUrl"
+$url = $_POST['url'];
+$oldUrl = "";
 
+// If session already contains old url set oldUrl to the old url in the session
+if (isset($_SESSION['oldUrl']))
+{
+	$oldUrl = $_SESSION['oldUrl'];
+}
+
+// If new url is not the same as old url, the call is valid (Prevent spam trough page reload on the client side)
+if ($url != $oldUrl)
+{
+	$connector = new Connector;
+	$connector->saveCall($url)
+	// Set new url as old url for the next call
+	$_SESSION['oldUrl'] = $url;
+}
 ?>
